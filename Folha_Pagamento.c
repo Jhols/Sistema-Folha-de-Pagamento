80e386e674c0bb25dbd3d8933cc296260b5112c7
João Pedro Hegouet <jphegouet@gmail.com>
2017-06-08 20:10:57 -0300

    Função "incluirFuncionario()" finalizado!
    - Terminada a primeira função do sistema.
    - A função irá inserir um novo funcionário ao vetor de funcionários do sistema.

--- Folha_Pagamento.c
+++ Folha_Pagamento.c
@@ -1,36 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+//Constantes
 #define MAX_FUNCS 20
+#define TAM_MATRICULA 10
 #define TAM_NOME 20
 #define TAM_DNASC 10
 
 //Registro de funcionarios
 typedef struct Funcionario {
-  char matricula[9];
+  char matricula[TAM_MATRICULA];
   float salBruto;
   char nome[TAM_NOME], dataNascimento[TAM_DNASC];
 }Funcionario;
 
 //Assinatura das funções
 void incluirFuncionario(Funcionario *vetor) {
-  int matricula, salBruto;
-  char nome[TAM_NOME], dataNascimento[TAM_DNASC];
+  //Declaração de um funcionário auxiliar
+  Funcionario func;
 
   //system("cls");
-  puts("Insira um número de Matrícula:");
-  scanf("%d", &matricula);
-  fflush(stdin);
-
+  printf("\nInsira um número de Matrícula:\n> ");
+  gets(func.matricula); //Captura a string digitada pelo usuário e atribui à matricula do funcionario auxiliar
+  setbuf(stdin, NULL); //Limpa o Buffer do Teclado de forma segura
   //Busca no vetor de funcionários se já existe algum registro com o número de matrícula inserido pelo usuário
   for (int i = 0; i < MAX_FUNCS; i++) {
-    if (vetor[i].matricula == matricula) {
-      puts("ERRO!!! Já existe um registro de funcionário com este num de matrícula!");
+    if (!strcmp(vetor[i].matricula, func.matricula)) {
+      puts("ERRO!!! Já existe um registro de funcionário com este número de matrícula!");
       getchar();
       return;
     }
   }
-  puts("que que isso"); getchar(); return;
+
+  //Inicio - Coleta dos dados do novo funcionário
+  puts("\nDigite o Nome do Funcionário:");
+  do {
+    printf("> ");
+    gets(func.nome);
+    setbuf(stdin, NULL);
+    if (!strcmp(func.nome, "")) { //Se o usuário não digitar um nome, o programa solicitará para pôr novamente
+      puts("Campo Obrigatório! Por favor, digite um nome:");
+    }
+  }while (!strcmp(func.nome, ""));
+
+  puts("\nDigite a Data de Nascimento: Formato[dd/mm/aaaa]");
+  do {
+    printf("> ");
+    gets(func.dataNascimento);
+    setbuf(stdin, NULL);
+    if (!strcmp(func.dataNascimento, "")) {
+      puts("Campo Obrigatório! Por favor, digite uma Data de Nascimento:");
+    }
+  }while (!strcmp(func.dataNascimento, ""));
+
+  puts("\nDigite o valor do Salário Bruto:");
+  do {
+    printf("> ");
+    scanf("%f", &func.salBruto);
+    setbuf(stdin, NULL);
+    if (func.salBruto < 0) {
+      puts("ERRO!!! Valor do Salário Bruto inválido! Por favor, insira um valor positivo.");
+    }
+  }while(func.salBruto < 0);
+  //Fim - Coleta dos dados do novo funcionário
+
+  //Busca o último elemento preenchido do vetor
+  int i=0;
+  while(vetor[i].matricula[0] != '\0')
+    i++;
+
+  //Inserção dos dados coletados no vetor de funcionários
+  strcpy(vetor[i].matricula, func.matricula);
+  strcpy(vetor[i].nome, func.nome);
+  strcpy(vetor[i].dataNascimento, func.dataNascimento);
+  vetor[i].salBruto = func.salBruto;
+
+  //Conclusão da inserção
+  puts("\nNovo funcionário adicionado!");
+  getchar();
+
 }
 
 //Função Principal
@@ -38,7 +87,6 @@ int main() {
 
   //Declaração de Variáveis
   Funcionario funcionarios[MAX_FUNCS];
-  funcionarios[3].matricula = 123456789;
   int opcao;
 
   //Menu Inicial
@@ -51,7 +99,7 @@ int main() {
     do {
       printf("> ");
       scanf("%d", &opcao);
-      fflush(stdin);
+      setbuf(stdin, NULL); //Limpa o Buffer do teclado de forma segura
 
       switch (opcao) {
         case 1:
